ba2a5c3eb26534ef85905aad0a5f90d97d3bd662
Dei-web <deilerc27@gmail.com>
2026-08-31 22:16:08 -0500

    fix: add nested routes to Admin component

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAdmin } from '../../context/AdminContext'
 import PasswordGate from './PasswordGate'
 import AdminLayout from './AdminLayout'
@@ -8,5 +9,15 @@ import CommentsTab from './CommentsTab'
 export default function Admin() {
   const { loggedIn } = useAdmin()
   if (!loggedIn) return <PasswordGate />
-  return <AdminLayout />
+  return (
+    <Routes>
+      <Route element={<AdminLayout />}>
+        <Route path="proyectos" element={<ProjectList />} />
+        <Route path="nuevo" element={<ProjectForm />} />
+        <Route path="editar/:id" element={<ProjectForm />} />
+        <Route path="comentarios" element={<CommentsTab />} />
+        <Route index element={<Navigate to="proyectos" replace />} />
+      </Route>
+    </Routes>
+  )
 }
